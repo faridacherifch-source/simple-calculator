f1cf9e791e181f629b1969d70e501e8f9df44b93
farida <farida.cherifch@gmail.com>
2026-09-02 02:58:59 +0300

    added new functionss

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -2,11 +2,15 @@
 #include <cmath>//math library for power function
 using namespace std;
 int main(){
-int n1 , n2 , result=0;//int the first and second number in case of decimals
+float angle=0; 
+float result3=0; // float as result may have decimals so must be stored in a double float just incase :)
+double result2=0; //for the square root because it holds large decimal values therfore the result must be stored in a double float
+float n3 ,n4;//float the division numbers as high chance they contain decimals
+ int n1 , n2 , result=0;//int the first and second number in case of decimals
 int function; //the function which will hole the value of the desired operation
 do{
 //use a do while loop so the computer keeps asking the user for input after each operation
-cout<<"1)Power"<<endl<<"2)Multiply"<<endl<<"3)divide"<<endl<<"4)addition"<<endl<<"5)Subtract"<<endl<<"0 to exit"<<endl<<"insert the number of your desired function:";
+cout<<"1)Power"<<endl<<"2)Multiply"<<endl<<"3)divide"<<endl<<"4)addition"<<endl<<"5)Subtract"<<endl<<"6)square root"<<endl<<"7)trigonometry"<<endl<<"8)log functions"<<endl<<"0 to exit"<<endl<<"insert the number of your desired function:";
 cin>>function;
 //ask the user for input and store it in function
 if (function==1){
@@ -34,7 +38,6 @@ else if (function==2){
 }
 else if (function==3){
 cout<<"insert first number: ";
-float n3 ,n4;//float the division numbers as high chance they contain decimals
 cin>>n3;
 cout<<"insert second number: ";
 cin>>n4; 
@@ -57,7 +60,78 @@ cin>>n2;
 result=n1-n2;
 cout<<"result:"<<result<<endl;
 }
-else ;}
+else if(function==6){
+cout<<"enter your number: ";
+cin>>n3;//store value in n3 as it is a float
+result2 = sqrt(n3);//function in math library for square root 
+cout<<"result= "<<result2<<endl;
+}
+else if (function==7){
+cout<<"1)sin"<<endl<<"2)cos"<<endl<<"3)tan"<<endl<<"4)cosec"<<endl<<"5)sec"<<endl<<"6)cotg"<<endl<<"insert the number of your desired function: ";
+cin>>function;
+if(function==1){
+cout<<"please input your desired angle: ";
+cin>>angle;
+double radians = angle * M_PI / 180.0;//the trig functions in C++ only take the angles in rad so this formula transfers the angle to rad
+result3= sin(radians);
+cout<<"result= "<<result3<<endl;
+}
+else if (function==2){
+double radians = angle * M_PI / 180.0;
+cout<<"please input your desired angle: ";
+cin>>angle;
+result3= cos(radians);
+cout<<"result= "<<result3<<endl;
+}
+else if (function==3){
+cout<<"please input your desired angle: ";
+cin>>angle;
+double radians = angle * M_PI / 180.0;
+result3= tan(radians);
+cout<<"result= "<<result3<<endl;
+}
+else if (function==4){
+cout<<"please input your desired angle: ";
+cin>>angle;
+double radians = angle * M_PI / 180.0;
+result3= asin(radians);//the asin or acos or atan signifies the inverse
+cout<<"result= "<<result3<<endl;}
+else if (function==5){
+cout<<"please input your desired angle: ";
+cin>>angle;
+double radians = angle * M_PI / 180.0;
+result3= acos(radians);
+cout<<"result= "<<result3<<endl;}
+else if (function==6){
+cout<<"please input your desired angle: ";
+cin>>angle;
+double radians = angle * M_PI / 180.0;
+result3= atan(radians);
+cout<<"result3= "<<result<<endl;}}
+else if(function==8){
+cout<<"1)ln"<<endl<<"2)log10"<<endl<<"3)log(custom base)"<<endl<<"please slect your desired function: ";
+cin>>function;
+if(function==1){
+cout<<"please input desired number";
+cin>>n1;
+result = log(n1);
+cout<<"result: "<<result<<endl;
+}
+else if(function==2){
+cout<<"please input desired number";
+cin>>n1;
+result = log10(n1);//log base 10
+cout<<"result: "<<result<<endl;
+}
+else if(function==3){
+cout<<"please input desired base: "<<endl;
+cin>>n1;
+cout<<"please input desired number: ";
+cin>>n2;
+result = log(n1) / log(n2);//in order for the user to select custom base use this formula
+cout<<"result: "<<result<<endl;}}
+else ;
+}
 while (function!=0);
 cout<<"goodbye :)";
 //the condition for while is function!= 0 which means the function dosent equal 0 
@@ -65,7 +139,6 @@ cout<<"goodbye :)";
 return 0;
 //to let the computer know that the program has ended
 }
-
 
 
 
