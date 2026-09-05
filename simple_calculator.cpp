d3885f977bfe7a220c1066899e5ba817058f6000
farida <farida.cherifch@gmail.com>
2026-09-05 18:28:53 +0300

    fixed a few errors
    fixed the following errors:
    1)wrong custom base log formula
    2)not storing the trig functions and log functions in result2(float)

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -6,7 +6,7 @@ float angle=0;
 float result3=0; // float as result may have decimals so must be stored in a double float just incase :)
 double result2=0; //for the square root because it holds large decimal values therfore the result must be stored in a double float
 float n3 ,n4;//float the division numbers as high chance they contain decimals
- int n1 , n2 , result=0;//int the first and second number in case of decimals
+ int n1 , n2 , result=0;//int the first and second number as they are used for the operations that most likely wont contain decimals
 int function; //the function which will hole the value of the desired operation
 do{
 //use a do while loop so the computer keeps asking the user for input after each operation
@@ -94,42 +94,42 @@ else if (function==4){
 cout<<"please input your desired angle: ";
 cin>>angle;
 double radians = angle * M_PI / 180.0;
-result3= asin(radians);//the asin or acos or atan signifies the inverse
+result3= 1/sin(radians);//one over the function to obtain its the inverse
 cout<<"result= "<<result3<<endl;}
 else if (function==5){
 cout<<"please input your desired angle: ";
 cin>>angle;
 double radians = angle * M_PI / 180.0;
-result3= acos(radians);
+result3= 1/cos(radians);
 cout<<"result= "<<result3<<endl;}
 else if (function==6){
 cout<<"please input your desired angle: ";
 cin>>angle;
 double radians = angle * M_PI / 180.0;
-result3= atan(radians);
+result3= 1/tan(radians);
 cout<<"result3= "<<result<<endl;}}
 else if(function==8){
-cout<<"1)ln"<<endl<<"2)log10"<<endl<<"3)log(custom base)"<<endl<<"please slect your desired function: ";
+cout<<"1)ln"<<endl<<"2)log10"<<endl<<"3)log(custom base)"<<endl<<"please select your desired function: ";
 cin>>function;
 if(function==1){
-cout<<"please input desired number";
-cin>>n1;
-result = log(n1);
-cout<<"result: "<<result<<endl;
+cout<<"please input desired number: ";
+cin>>n3;
+result2 = log(n3);
+cout<<"result: "<<result2<<endl;
 }
 else if(function==2){
 cout<<"please input desired number";
-cin>>n1;
-result = log10(n1);//log base 10
-cout<<"result: "<<result<<endl;
+cin>>n3;
+result2 = log10(n3);//log base 10
+cout<<"result: "<<result2<<endl;
 }
 else if(function==3){
-cout<<"please input desired base: "<<endl;
-cin>>n1;
+cout<<"please input desired base: ";
+cin>>n3;
 cout<<"please input desired number: ";
-cin>>n2;
-result = log(n1) / log(n2);//in order for the user to select custom base use this formula
-cout<<"result: "<<result<<endl;}}
+cin>>n4;
+result2 = log(n4) / log(n3);//in order for the user to select custom base use this formula
+cout<<"result: "<<result2<<endl;}}
 else ;
 }
 while (function!=0);
